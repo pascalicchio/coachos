30767fc761c32ac6228575f2beefcaf26623fc2e
root <root@srv1313547.hstgr.cloud>
2026-02-19 11:35:27 -0500

    Fix seeder: remove duplicate plan/role creation (handled by migrations)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,22 +13,7 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        // Plans
-        $plans = [
-            ['name' => 'Starter', 'price' => 19, 'max_locations' => 1, 'max_coaches' => 5, 'features' => ['Basic scheduling', '50 members', 'Email support']],
-            ['name' => 'Pro', 'price' => 39, 'max_locations' => 3, 'max_coaches' => 15, 'features' => ['Advanced scheduling', '200 members', 'Priority support', 'SMS notifications']],
-            ['name' => 'Business', 'price' => 79, 'max_locations' => 999, 'max_coaches' => 999, 'features' => ['Unlimited everything', 'API access', '24/7 support', 'Custom branding']],
-        ];
-
-        foreach ($plans as $plan) {
-            Plan::create($plan);
-        }
-
-        // Roles
-        $roles = ['owner', 'manager', 'coach', 'staff'];
-        foreach ($roles as $role) {
-            Role::create(['name' => $role]);
-        }
+        // Plans and Roles are seeded via migrations
 
         // Demo Organization
         $org = Organization::create([
